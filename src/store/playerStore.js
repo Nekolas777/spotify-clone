4f7a226562f25bc03aabecf9dbc61b1141931cbf
Nicolas Esteban <u202217485@upc.edu.pe>
2025-03-28 01:54:59 -0500

    feat: initialize current music with default playlist and song, add progress state

--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -1,14 +1,17 @@
 import { create } from "zustand";
+import { playlists } from "../data/playlists";
+import { songs } from "../data/songs";
 
 export const usePlayerStore = create((set) => ({
   isPlaying: false,
+  progress: 0,
   currentMusic: {
-    playlist: null,
-    song: null,
-    songs: [],
+    playlist: playlists[0],
+    song: songs[0],
   },
   volume: 1,
+  setProgress: (progress) => set({ progress }),
   setVolume: (volume) => set({ volume }),
   setIsPlaying: (isPlaying) => set({ isPlaying }),
-  setCurrentMusic: (currentMusic) => set({ currentMusic })
+  setCurrentMusic: (currentMusic) => set({ currentMusic }),
 }));
