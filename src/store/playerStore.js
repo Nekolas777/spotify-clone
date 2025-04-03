9b569af858f2f1bef2014e9565b8d4e837f562a9
Nicolas Esteban <u202217485@upc.edu.pe>
2025-04-03 00:50:05 -0500

    feat: add favorite track management to player store and local storage utilities

--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -10,6 +10,7 @@ export const usePlayerStore = create((set) => ({
     playlist: playlists[0],
     song: songs[0],
   },
+  tracks: [],
   volume: 100,
   audioRef: React.createRef(), // creamos una referencia de audio por defecto para evitar al setear una referencia vacia
   setAudioRef: (ref) => set({ audioRef: ref }),
@@ -17,4 +18,13 @@ export const usePlayerStore = create((set) => ({
   setVolume: (volume) => set({ volume }),
   setIsPlaying: (isPlaying) => set({ isPlaying }),
   setCurrentMusic: (currentMusic) => set({ currentMusic }),
+  setTracks: (updatedSongs) => set({ tracks: updatedSongs }),
+  // method to toggle the favorite song
+  createFavoriteSong: (songId, isFav) =>
+    set((state) => {
+      const updatedTracks = state.tracks.map((song) =>
+        song.id === songId ? { ...song, isFavorite: !song.isFavorite } : song
+      );
+      return { tracks: updatedTracks };
+    }),
 }));
