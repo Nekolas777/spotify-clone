d2ad07a2aa7e4c873ed3076df5e9fbd1b9611d25
Nicolas Esteban <u202217485@upc.edu.pe>
2025-04-03 00:49:44 -0500

    feat: implement favorite track functionality in CurrentSong component

--- src/components/Player/CurrentSong.jsx
+++ src/components/Player/CurrentSong.jsx
@@ -1,22 +1,51 @@
+import { getFavoritesTracks, isFavoriteTrack, toggleFavoriteTrack } from "src/utils/helpers/localStorage";
 import { Heart } from "../../icons/Heart";
 import { usePlayerStore } from "../../store/playerStore";
+import FormatArtists from "../FormatArtists";
+import { useEffect, useState } from "react";
 
 export const CurrentSong = () => {
+  const { currentMusic, createFavoriteSong } = usePlayerStore();
 
-  const { currentMusic } = usePlayerStore();
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleFavoriteTrack = () => {
+    toggleFavoriteTrack(currentMusic.song.id);
+    createFavoriteSong(currentMusic.song.id);
+    setIsFavorite(!isFavorite);
+  };
+
+  useEffect(() => {
+    const hasFavoriteTrack = isFavoriteTrack(currentMusic.song.id);
+    setIsFavorite(hasFavoriteTrack);
+  }, [currentMusic, createFavoriteSong]);
+
 
   return (
-    <div className="flex flex-row gap-4 items-center">
+    <div className='flex flex-row gap-4 items-center'>
       <img
         src={currentMusic?.song?.bannerImage}
-        alt="banner_image"
-        className="w-16 h-16 rounded-[4px]"
+        alt='banner_image'
+        className='w-16 h-16 rounded-[4px]'
       />
-      <div className="flex flex-col justify-center gap-1">
-        <h3 className="text-sm font-medium tracking-wide">{currentMusic?.song?.title}</h3>
-        <span className="text-slate-300 text-xs">{currentMusic?.song?.album}</span>
+      <div className='flex flex-col justify-center gap-1'>
+        <h3 className='text-sm font-medium tracking-wide'>
+          {currentMusic?.song?.title}
+        </h3>
+        <div className='text-slate-300 text-xs'>
+          <FormatArtists artists={currentMusic?.song?.artists} />
+        </div>
       </div>
-      <Heart/>
+      <figure
+        onClick={handleFavoriteTrack}
+        className={`cursor-pointer ${
+          isFavorite
+            ? "text-green-500 fill-green-500"
+            : "text-gray-300 fill-none"
+        }`}
+      >
+        <Heart />
+      </figure>
     </div>
   );
 };
