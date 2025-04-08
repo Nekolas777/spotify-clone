817bbb43e917225d357784c4da0f31fd24225041
Nicolas Esteban <u202217485@upc.edu.pe>
2025-04-08 00:32:03 -0500

    fix: update CurrentSong component for improved layout and responsiveness

--- src/components/Player/CurrentSong.jsx
+++ src/components/Player/CurrentSong.jsx
@@ -23,23 +23,23 @@ export const CurrentSong = () => {
 
 
   return (
-    <div className='flex flex-row gap-4 items-center'>
+    <div className='flex flex-row gap-4 items-center max-w-fit md:max-w-[265px] w-full truncate'>
       <img
         src={currentMusic?.song?.bannerImage}
         alt='banner_image'
-        className='w-16 h-16 rounded-[4px]'
+        className='max-w-14 xs:max-w-16 max-h-14 xs:max-h-16 rounded-[4px]'
       />
-      <div className='flex flex-col justify-center gap-1'>
+      <div className='hidden xs:flex flex-col justify-center gap-1'>
         <h3 className='text-sm font-medium tracking-wide'>
           {currentMusic?.song?.title}
         </h3>
-        <div className='text-slate-300 text-xs'>
+        <div className='text-slate-300 text-xs '>
           <FormatArtists artists={currentMusic?.song?.artists} />
         </div>
       </div>
       <figure
         onClick={handleFavoriteTrack}
-        className={`cursor-pointer ${
+        className={`hidden md:block cursor-pointer ${
           isFavorite
             ? "text-green-500 fill-green-500"
             : "text-gray-300 fill-none"
