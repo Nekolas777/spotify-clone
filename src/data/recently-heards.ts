547439efc73e26fc200c6c97db6474d6188a4991
Nicolas Esteban <u202217485@upc.edu.pe>
2025-04-08 00:32:26 -0500

    feat: add RecentlyHeards interface and sample data

---
+++ src/data/recently-heards.ts
@@ -0,0 +1,39 @@
+export interface RecentlyHeards {
+  id: number;
+  title: string;
+  description: string;
+  bannerImg: string;
+}
+
+export const recentlyHeards: RecentlyHeards[] = [
+  {
+    id: 1,
+    title: 'Armonia 10',
+    description: 'Con Bacilos, Caifanes, Vilma Palma e Vampiros y más',
+    bannerImg: 'https://thisis-images.spotifycdn.com/37i9dQZF1DZ06evO1BtD22-default.jpg',
+  },
+  {
+    id: 2,
+    title: 'Tu falta de Querer',
+    description: 'Con Mon Laferte, Julieta Venegas, León Larregui y más',
+    bannerImg: 'https://i.scdn.co/image/ab67616d00001e027e709ce66e549bae1f3983ce',
+  },
+  {
+    id: 3,
+    title: "Funkin' Sound Team",
+    description: "Con YingYang48, atsuover, PaperKitty y más",
+    bannerImg: 'https://i.scdn.co/image/ab67616d00001e02e2b2d57a11b4b222de9314ac',
+  },
+  {
+    id: 4,
+    title: 'Big Time Rush',
+    description: "Con Victorious Cast, Christopher Wilde, Bridgit Mendler y más",
+    bannerImg: 'https://i.scdn.co/image/ab67616d00001e0288b997e9df48526ca968915b',
+  },
+  {
+    id: 5,
+    title: 'Caravan Place',
+    description: "Con The Electric Swing Circus, Pisk, Tape Five y más",
+    bannerImg: 'https://i.scdn.co/image/ab67616d00001e02d4189ca2964e92b5e5e545ed',
+  },
+];
